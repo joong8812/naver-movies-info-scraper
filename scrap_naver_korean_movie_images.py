4f2d374c4486c9b0b9231fd8d637449de325dbc7
jungjaechoi <joong8812@gmail.com>
2022-02-03 21:02:38 +0900

    add - 네이버 한국 영화 포스터(이미지) 다운로드

---
+++ scrap_naver_korean_movie_images.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import urllib.request
+
+korean_movie_table = pd.read_csv('./naver_korean_movie_list.csv', encoding='utf-8', sep=',') # csv파일 불러오기s
+for i, movie in enumerate(korean_movie_table.values):
+    movie_title = movie[0]
+    movie_code = movie[1]
+    print(f'##### {i+1} ##### {movie_code} {movie_title}')
+
+    url = f"https://movie.naver.com/movie/bi/mi/photoViewPopup.naver?movieCode={movie_code}"
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+    image_source = soup.select('#page_content a img')[0]['src']
+
+    # image request & download
+    urllib.request.urlretrieve(image_source, f'naver_korean_movie_images/movie_{movie_code}.jpg')
